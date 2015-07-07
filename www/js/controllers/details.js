2abf016773328be54817582e21b2a6c23ae8abb9
Javier Pena <javier@javierdev.com>
2015-07-07 01:08:57 -0500

    Layed out details more.

--- www/js/controllers/details.js
+++ www/js/controllers/details.js
@@ -8,6 +8,8 @@ angular.module('weatherHistory.controllers')
           forecast.data.daily.data[0].temperatureMinTime = moment(forecast.data.daily.data[0].temperatureMinTime).format('hh:mm')
           forecast.data.daily.data[0].sunriseTime = moment(forecast.data.daily.data[0].sunriseTime).format('hh:mm')
           forecast.data.daily.data[0].sunsetTime = moment(forecast.data.daily.data[0].sunsetTime).format('hh:mm')
+          forecast.data.year = parseInt(moment.unix(forecast.data.currently.time).format('YYYY'), 10);
+          forecast.data.currently.icon = forecastFactory.renameIcons(forecast.data.currently.icon);
           $scope.details = forecast.data;
         });
     });
