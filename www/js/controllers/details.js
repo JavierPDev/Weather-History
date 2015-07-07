7bf2e275061da4d4f6371aff12db62e475ea052b
Javier Pena <javier@javierdev.com>
2015-07-07 01:51:30 -0500

    Fixed times formatting in details.

--- www/js/controllers/details.js
+++ www/js/controllers/details.js
@@ -4,14 +4,23 @@ angular.module('weatherHistory.controllers')
     .then(function(settings) {
       forecastFactory.getForecast(settings.latitude, settings.longitude, $state.params.time, settings)
         .then(function (forecast) {
-          forecast.data.daily.data[0].temperatureMaxTime = moment(forecast.data.daily.data[0].temperatureMaxTime).format('hh:mm')
-          forecast.data.daily.data[0].temperatureMinTime = moment(forecast.data.daily.data[0].temperatureMinTime).format('hh:mm')
-          forecast.data.daily.data[0].sunriseTime = moment(forecast.data.daily.data[0].sunriseTime).format('hh:mm')
-          forecast.data.daily.data[0].sunsetTime = moment(forecast.data.daily.data[0].sunsetTime).format('hh:mm')
+          forecast.data.daily.data[0].temperatureMaxTime = formatDate(forecast.data.daily.data[0].temperatureMaxTime);
+          forecast.data.daily.data[0].temperatureMinTime = formatDate(forecast.data.daily.data[0].temperatureMinTime);
+          forecast.data.daily.data[0].sunriseTime = formatDate(forecast.data.daily.data[0].sunriseTime);
+          forecast.data.daily.data[0].sunsetTime = formatDate(forecast.data.daily.data[0].sunsetTime);
           forecast.data.year = parseInt(moment.unix(forecast.data.currently.time).format('YYYY'), 10);
           forecast.data.currently.icon = forecastFactory.renameIcons(forecast.data.currently.icon);
           $scope.details = forecast.data;
         });
     });
+
+  function formatDate(date) {
+    // Check if date needs to be formatted or if formatted date was cached
+    if (date.toString().indexOf('m') > -1) {
+      return date;
+    } else {
+      return moment(date * 1000).format('hh:mma');
+    }
+  }
 });
 
