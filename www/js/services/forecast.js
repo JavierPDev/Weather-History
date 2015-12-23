ac37416ccf87071e12890e2385d6d015507d5e6d
Javier Pena <javier@javierdev.com>
2015-12-23 12:21:39 -0600

    Cleaned up services

--- www/js/services/forecast.js
+++ www/js/services/forecast.js
@@ -1,62 +1,71 @@
-/**
- * ForecastIO service module
- * @module services/
- */
-angular.module('weatherHistory.services')
-.factory('forecastFactory', function($http, $q, CacheFactory, FORECASTIO_API_KEY) {
-  var baseUrl = 'https://api.forecast.io/forecast/'+FORECASTIO_API_KEY+'/',
-    MIN = 1000 * 60,
-    forecastCache = CacheFactory.createCache('forecastCache', {
-      maxAge: 15 * MIN,
-      deleteOnExpire: 'aggressive'
-    });
+(function() {
+  'use strict';
 
-  /**
-   * Get forecast for a location at a certain time.
-   *
-   * @param {Number} latitude - Latitude
-   * @param {Number} longitude - Longitude
-   * @param {Number} time - Unix timestamp in seconds
-   * @param {Object} options - Options to pass as query parameters
-   * @return {Object} promise - $http future object
-   */
-  function getForecast(latitude, longitude, time, options) {
-    var deferred = $q.defer();
-    options.callback = 'JSON_CALLBACK';
+  angular
+    .module('weatherHistory.services')
+    .factory('forecastFactory', forecastFactory);
 
-    return $http.jsonp(baseUrl+latitude+','+longitude+','+time, {
-      cache: forecastCache,
-      params: options
-    });
+  forecastFactory.$inject = [
+    '$http',
+    '$q',
+    'CacheFactory',
+    'FORECASTIO_API_KEY'
+  ];
+
+  function forecastFactory($http, $q, CacheFactory, FORECASTIO_API_KEY) {
+    var baseUrl = 'https://api.forecast.io/forecast/'+FORECASTIO_API_KEY+'/',
+      MIN = 1000 * 60,
+      forecastCache = CacheFactory.createCache('forecastCache', {
+        maxAge: 15 * MIN,
+        deleteOnExpire: 'aggressive'
+      });
+
+    /**
+     * Get forecast for a location at a certain time.
+     *
+     * @param {Number} latitude - Latitude
+     * @param {Number} longitude - Longitude
+     * @param {Number} time - Unix timestamp in seconds
+     * @param {Object} options - Options to pass as query parameters
+     * @return {Object} promise - $http future object
+     */
+    function getForecast(latitude, longitude, time, options) {
+      var deferred = $q.defer();
+      options.callback = 'JSON_CALLBACK';
+
+      return $http.jsonp(baseUrl+latitude+','+longitude+','+time, {
+        cache: forecastCache,
+        params: options
+      });
+    }
+
+    /**
+     * Clear forecast cache so fresh data can be available.
+     */
+    function clearCache() {
+      forecastCache.removeAll();
+    }
+
+    /**
+     * Method for renaming icon strings from forecast.io to fit weather icons set.
+     *
+     * @param {String} iconStr - String representing what icon to display. From forecast.io
+     * @return {String} iconStr - String representing what icon to display. Modified for icon set
+     */
+    function renameIcons(iconStr) {
+      iconStr = iconStr.replace('partly-', '');
+      iconStr = iconStr.replace('wind', 'windy');
+      iconStr = iconStr.replace('clear-day', 'day-sunny');
+      iconStr = iconStr.replace('clear-night', 'night-clear');
+      iconStr = iconStr.replace('cloudy-night', 'night-cloudy');
+      iconStr = iconStr.replace('-day', '');
+      return iconStr;
+    }
+
+    return {
+      getForecast: getForecast,
+      clearCache: clearCache,
+      renameIcons: renameIcons
+    };
   }
-
-  /**
-   * Clear forecast cache so fresh data can be available.
-   */
-  function clearCache() {
-    forecastCache.removeAll();
-  }
-
-  /**
-   * Method for renaming icon strings from forecast.io to fit weather icons set.
-   *
-   * @param {String} iconStr - String representing what icon to display. From forecast.io
-   * @return {String} iconStr - String representing what icon to display. Modified for icon set
-   */
-  function renameIcons(iconStr) {
-    iconStr = iconStr.replace('partly-', '');
-    iconStr = iconStr.replace('wind', 'windy');
-    iconStr = iconStr.replace('clear-day', 'day-sunny');
-    iconStr = iconStr.replace('clear-night', 'night-clear');
-    iconStr = iconStr.replace('cloudy-night', 'night-cloudy');
-    iconStr = iconStr.replace('-day', '');
-    return iconStr;
-  }
-
-  return {
-    getForecast: getForecast,
-    clearCache: clearCache,
-    renameIcons: renameIcons
-  };
-});
-
+})();
--- www/js/services/geocoder.js
+++ www/js/services/geocoder.js
@@ -1,122 +1,135 @@
-angular.module('weatherHistory.services')
-.factory('geocoder', function($q, $http, CacheFactory, GOOGLE_API_KEY) {
-  var geocoder = new google.maps.Geocoder(),
-    deferredGeocode = $q.defer(),
-    deferredLocation = $q.defer(),
-    MIN = 60 * 1000,
-    timezoneCache = CacheFactory.createCache('timezoneCache', {
-      maxAge: 15 * MIN,
-      deleteOnExpire: 'aggressive'
-    }),
-    timezoneApiUrl = 'https://maps.googleapis.com/maps/api/timezone/json';
+(function() {
+  'use strict';
 
-  /**
-   * Use google maps API to get coordinates from an address.
-   *
-   * @param {String} addr Address
-   * @return {Object} Promise containing coordinates
-   */
-  function getGeocode(addr) {
-    geocoder.geocode({address: addr}, function(data, status) {
-      if(status === 'OK') {
-        var coords = {
-          latitude: parseFloat(data[0].geometry.location.k, 10),
-          longitude: parseFloat(data[0].geometry.location.B, 10)
-        };
+  angular
+    .module('weatherHistory.services')
+    .factory('geocoder', geocoder);
 
-        deferredGeocode.resolve(coords);
-      } else {
-        deferredGeocode.reject();
-      }
-    });
+  geocoder.$inject = [
+    '$q',
+    '$http',
+    'CacheFactory',
+    'GOOGLE_API_KEY'
+  ];
 
-    return deferredGeocode.promise;
-  }
+  function geocoder($q, $http, CacheFactory, GOOGLE_API_KEY) {
+    var geocoder = new google.maps.Geocoder(),
+      deferredGeocode = $q.defer(),
+      deferredLocation = $q.defer(),
+      MIN = 60 * 1000,
+      timezoneCache = CacheFactory.createCache('timezoneCache', {
+        maxAge: 15 * MIN,
+        deleteOnExpire: 'aggressive'
+      }),
+      timezoneApiUrl = 'https://maps.googleapis.com/maps/api/timezone/json';
 
-  /**
-   * Use google maps API to get a location from coordinates.
-   *
-   * @param {Number} latitud Latitude
-   * @param {Number} longitude Longitude
-   * @return {Object} Promise containing location
-   */
-  function getLocation(latitude, longitude) {
-    var latLon = new google.maps.LatLng(latitude, longitude);
+    /**
+     * Use google maps API to get coordinates from an address.
+     *
+     * @param {String} addr Address
+     * @return {Object} Promise containing coordinates
+     */
+    function getGeocode(addr) {
+      geocoder.geocode({address: addr}, function(data, status) {
+        if(status === 'OK') {
+          var coords = {
+            latitude: parseFloat(data[0].geometry.location.k, 10),
+            longitude: parseFloat(data[0].geometry.location.B, 10)
+          };
 
-    geocoder.geocode({'latLng': latLon}, function(location, status) {
-      if (status == google.maps.GeocoderStatus.OK && location.length > 0) {
-        deferredLocation.resolve(location[0]);
-      } else {
-        deferredLocation.reject();
-      }
-    });
+          deferredGeocode.resolve(coords);
+        } else {
+          deferredGeocode.reject();
+        }
+      });
 
-    return deferredLocation.promise;
-  }
+      return deferredGeocode.promise;
+    }
 
-  function getTimezone(latitude, longitude, date) {
-    var location = latitude+','+longitude,
-      deferred = $q.defer();
+    /**
+     * Use google maps API to get a location from coordinates.
+     *
+     * @param {Number} latitud Latitude
+     * @param {Number} longitude Longitude
+     * @return {Object} Promise containing location
+     */
+    function getLocation(latitude, longitude) {
+      var latLon = new google.maps.LatLng(latitude, longitude);
 
-    $http.get(timezoneApiUrl, {
-      cache: timezoneCache,
-      params: {
-        key: GOOGLE_API_KEY,
-        location: location,
-        timestamp: moment(date).unix()
-      }
-    })
-    .success(function(timezone) {
-      var offsetInSeconds = timezone.dstOffset + timezone.rawOffset,
-        negative = offsetInSeconds.toString().indexOf('-') > -1,
-        ZZ,
-        hh,
-        mm;
-      // Format timezone correctly for forecast io: +|-hhmm
-      offsetInSeconds = offsetInSeconds.toString().replace('-', '');
-      ZZ = moment.duration({seconds: offsetInSeconds});
-      hh = ZZ.hours();
-      hh = hh.toString().length < 2 ? '0'+hh.toString() : hh.toString();
-      mm = ZZ.minutes();
-      mm = mm.toString().length < 2 ? '0'+mm.toString() : mm.toString();
-      ZZ = hh.toString() + mm.toString();
-      ZZ = negative ? '-'+ZZ : '+'+ZZ;
-      timezone.offset = ZZ;
-      deferred.resolve(timezone);
-    });
+      geocoder.geocode({'latLng': latLon}, function(location, status) {
+        if (status == google.maps.GeocoderStatus.OK && location.length > 0) {
+          deferredLocation.resolve(location[0]);
+        } else {
+          deferredLocation.reject();
+        }
+      });
 
-    return deferred.promise;
-  }
+      return deferredLocation.promise;
+    }
 
-  /**
-   * Parse address components gotten from google maps places api.
-   *
-   * @param {Array} components Components
-   * @return {Object} components City and country
-   */
-  function parseAddressComponents(components) {
-    var city, country;
+    function getTimezone(latitude, longitude, date) {
+      var location = latitude+','+longitude,
+        deferred = $q.defer();
 
-    angular.forEach(components, function(component) {
-      if (component.types[0] === 'locality') {
-        city = component.long_name;
-      }
-      if (component.types[0] === 'country') {
-        country = component.long_name;
-      }
-    });
+      $http.get(timezoneApiUrl, {
+        cache: timezoneCache,
+        params: {
+          key: GOOGLE_API_KEY,
+          location: location,
+          timestamp: moment(date).unix()
+        }
+      })
+      .success(function(timezone) {
+        var offsetInSeconds = timezone.dstOffset + timezone.rawOffset,
+          negative = offsetInSeconds.toString().indexOf('-') > -1,
+          ZZ,
+          hh,
+          mm;
+        // Format timezone correctly for forecast io: +|-hhmm
+        offsetInSeconds = offsetInSeconds.toString().replace('-', '');
+        ZZ = moment.duration({seconds: offsetInSeconds});
+        hh = ZZ.hours();
+        hh = hh.toString().length < 2 ? '0'+hh.toString() : hh.toString();
+        mm = ZZ.minutes();
+        mm = mm.toString().length < 2 ? '0'+mm.toString() : mm.toString();
+        ZZ = hh.toString() + mm.toString();
+        ZZ = negative ? '-'+ZZ : '+'+ZZ;
+        timezone.offset = ZZ;
+        deferred.resolve(timezone);
+      });
+
+      return deferred.promise;
+    }
+
+    /**
+     * Parse address components gotten from google maps places api.
+     *
+     * @param {Array} components Components
+     * @return {Object} components City and country
+     */
+    function parseAddressComponents(components) {
+      var city, country;
+
+      angular.forEach(components, function(component) {
+        if (component.types[0] === 'locality') {
+          city = component.long_name;
+        }
+        if (component.types[0] === 'country') {
+          country = component.long_name;
+        }
+      });
+
+      return {
+        city: city,
+        country: country
+      };
+    }
 
     return {
-      city: city,
-      country: country
+      getGeocode: getGeocode,
+      getLocation: getLocation,
+      getTimezone: getTimezone,
+      parseAddressComponents: parseAddressComponents
     };
   }
-
-  return {
-    getGeocode: getGeocode,
-    getLocation: getLocation,
-    getTimezone: getTimezone,
-    parseAddressComponents: parseAddressComponents
-  };
-});
-
+})();
